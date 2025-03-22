51e83e2107787f243d3665aa24282dbba8beab43
N1-N4 <nixie1104@gmail.com>
2025-03-22 19:37:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Scene setup
+// Scenesetup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
