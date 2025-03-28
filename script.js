735328cdb18752ac1e4861b7bd3887b96efa4309
N1-N4 <nixie1104@gmail.com>
2025-03-28 14:21:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xf0e6d2);
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 10, 20);
+camera.position.set(0, 5, 20);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
